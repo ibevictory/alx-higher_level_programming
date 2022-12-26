c8f41e9e8c4e3f93b4c81fa97a15d5a000595f86
ibevictory <ibevictory50@gmail.com>
2022-12-26 14:41:50 +0000

    updated files

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+  """Representing a Square"""
+
+  def __init__(self, size):
+
+    """Initialization of a new square.
+    Args:
+        size(int): The size pf a square
+    """
+    self.__size = 
